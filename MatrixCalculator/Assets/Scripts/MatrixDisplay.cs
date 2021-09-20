b310246bfcb20acaaf9cb706cb91e77fee7c5221
unknown <kristevalex@gmail.com>
2021-09-21 00:39:43 +0300

    MatrixDisplay

---
+++ MatrixCalculator/Assets/Scripts/MatrixDisplay.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatrixDisplay : MatrixUI
+{
+    [SerializeField]
+    GameObject displayCellPrefab;
+    [SerializeField]
+    GameObject forCells;
+
+    Vector2 matrixSize;
+    GridLayoutGroup grid;
+
+    Text[,] matrixCells;
+
+    void Start()
+    {
+        MatrixCalc.Matrix _matrix = new MatrixCalc.Matrix(2, 3);
+        for (int i = 0; i < _matrix.rows; i++)
+            for (int j = 0; j < _matrix.columns; j++)
+                _matrix.data[i, j] = 10 * i + j;
+
+        grid = forCells.GetComponent<GridLayoutGroup>();
+
+        matrixSize = forCells.GetComponent<RectTransform>().sizeDelta;
+
+        UpdateMatrix(_matrix);
+    }
+
+    void UpdateDisplay()
+    {
+        for (int i = 0; i < matrix.rows; i++)
+            for (int j = 0; j < matrix.columns; j++)
+                matrixCells[i, j].text = matrix.data[i, j].ToString();
+    }
+
+    void UpdateMatrix(MatrixCalc.Matrix _matrix)
+    {
+        matrix = _matrix;
+
+        foreach (Transform child in forCells.transform)
+            GameObject.Destroy(child.gameObject);
+
+        matrixCells = new Text[matrix.rows, matrix.columns];
+
+        grid.constraintCount = matrix.columns;
+        grid.cellSize = new Vector2(0.9f * (matrixSize.x - 2 * 10) / matrix.columns, 0.9f * (matrixSize.y - 2 * 10) / matrix.rows);
+        grid.spacing = new Vector2(0.1f * (matrixSize.x - 2 * 10) / matrix.columns, 0.1f * (matrixSize.y - 2 * 10) / matrix.rows);
+        grid.padding = new RectOffset(10, 10, 10, 10);
+
+        for (int i = 0; i < matrix.rows; i++)
+        {
+            for (int j = 0; j < matrix.columns; j++)
+            {
+                GameObject tmpCell = Instantiate(displayCellPrefab);
+                tmpCell.transform.SetParent(forCells.transform);
+                tmpCell.name = "Cell (" + i + "," + j + ")";
+                tmpCell.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+                matrixCells[i, j] = tmpCell.GetComponentInChildren<Text>();
+            }
+        }
+
+        UpdateDisplay();
+    }
+}
---
+++ MatrixCalculator/Assets/Scripts/MatrixUI.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class MatrixUI : MonoBehaviour
+{
+    public MatrixCalc.Matrix matrix;
+}
