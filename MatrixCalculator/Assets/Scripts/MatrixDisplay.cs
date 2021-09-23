623993676ddaa30813f1a5f061bcd6c6a9f2701e
unknown <kristevalex@gmail.com>
2021-09-23 18:46:18 +0300

    More matrix display logic moved to base matrix ui class

--- MatrixCalculator/Assets/Scripts/MatrixDisplay.cs
+++ MatrixCalculator/Assets/Scripts/MatrixDisplay.cs
@@ -3,73 +3,23 @@ using UnityEngine.UI;
 
 public class MatrixDisplay : MatrixUI
 {
-    [SerializeField]
-    GameObject displayCellPrefab;
-    [SerializeField]
-    GameObject forCells;
-
-    Vector2 matrixSize;
-    GridLayoutGroup grid;
-
     Text[,] matrixCells;
+    
 
-    void Start()
+    override protected void ResizeCells(int rows, int columns)
     {
-        MatrixCalc.Matrix _matrix = new MatrixCalc.Matrix(2, 3);
-        for (int i = 0; i < _matrix.rows; i++)
-            for (int j = 0; j < _matrix.columns; j++)
-                _matrix.data[i, j] = 10 * i + j;
-
-        grid = forCells.GetComponent<GridLayoutGroup>();
-
-        matrixSize = forCells.GetComponent<RectTransform>().sizeDelta;
-
-        UpdateMatrix(_matrix);
+        matrixCells = new Text[rows, columns];
     }
 
-    void UpdateDisplay()
+    override protected void AssignToCell(int row, int column, GameObject toAssign)
+    {
+        matrixCells[row, column] = toAssign.GetComponentInChildren<Text>();
+    }
+
+    override protected void UpdateDisplay()
     {
         for (int i = 0; i < matrix.rows; i++)
             for (int j = 0; j < matrix.columns; j++)
                 matrixCells[i, j].text = matrix.data[i, j].ToString();
-    }
-
-    override public void ResizeMatrix(int rows, int colomns)
-    {
-        matrix = new MatrixCalc.Matrix(rows, colomns);
-
-        foreach (Transform child in forCells.transform)
-            GameObject.Destroy(child.gameObject);
-
-        matrixCells = new Text[matrix.rows, matrix.columns];
-
-        grid.constraintCount = matrix.columns;
-        grid.cellSize = new Vector2(0.9f * (matrixSize.x - 2 * 10) / matrix.columns, 0.9f * (matrixSize.y - 2 * 10) / matrix.rows);
-        grid.spacing = new Vector2(0.1f * (matrixSize.x - 2 * 10) / matrix.columns, 0.1f * (matrixSize.y - 2 * 10) / matrix.rows);
-        grid.padding = new RectOffset(10, 10, 10, 10);
-
-        for (int i = 0; i < matrix.rows; i++)
-        {
-            for (int j = 0; j < matrix.columns; j++)
-            {
-                GameObject tmpCell = Instantiate(displayCellPrefab);
-                tmpCell.transform.SetParent(forCells.transform);
-                tmpCell.name = "Cell (" + i + "," + j + ")";
-                tmpCell.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-
-                matrixCells[i, j] = tmpCell.GetComponentInChildren<Text>();
-            }
-        }
-
-        UpdateDisplay();
-    }
-
-    override public void UpdateMatrix(MatrixCalc.Matrix _matrix)
-    {
-        if (matrix.rows != _matrix.rows || matrix.columns != _matrix.columns)
-            return;
-
-        matrix = _matrix;
-        UpdateDisplay();
     }
 }
--- MatrixCalculator/Assets/Scripts/MatrixUI.cs
+++ MatrixCalculator/Assets/Scripts/MatrixUI.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class MatrixUI : MonoBehaviour
 {
+    [SerializeField]
+    GameObject cellObjectPrefab;
+    [SerializeField]
+    GameObject forCells;
+
+    Vector2 matrixSize;
+    GridLayoutGroup grid;
+
     public MatrixCalc.Matrix matrix { get; protected set; }
 
-    abstract public void ResizeMatrix(int rows, int colomns);
 
-    abstract public void UpdateMatrix(MatrixCalc.Matrix _matrix);
-
-    public void ResizeAndUpdateMatrix(MatrixCalc.Matrix _matrix)
+    void Start()
     {
-        ResizeMatrix(_matrix.rows, _matrix.columns);
-        UpdateMatrix(_matrix);
+        MatrixCalc.Matrix _matrix = new MatrixCalc.Matrix(2, 3);
+        for (int i = 0; i < _matrix.rows; i++)
+            for (int j = 0; j < _matrix.columns; j++)
+                _matrix.data[i, j] = 10 * i + j;
+
+        grid = forCells.GetComponent<GridLayoutGroup>();
+
+        matrixSize = forCells.GetComponent<RectTransform>().sizeDelta;
+
+        SetMatrixValue(_matrix);
+    }
+
+    abstract protected void ResizeCells(int rows, int columns);
+
+    abstract protected void AssignToCell(int row, int column, GameObject toAssign);
+
+    abstract protected void UpdateDisplay();
+
+    public void ResizeMatrix(int rows, int columns)
+    {
+        matrix = new MatrixCalc.Matrix(rows, columns);
+
+        foreach (Transform child in forCells.transform)
+            GameObject.Destroy(child.gameObject);
+
+        ResizeCells(rows, columns);
+
+        grid.constraintCount = matrix.columns;
+        grid.cellSize = new Vector2(0.9f * (matrixSize.x - 2 * 10) / matrix.columns, 0.9f * (matrixSize.y - 2 * 10) / matrix.rows);
+        grid.spacing = new Vector2(0.1f * (matrixSize.x - 2 * 10) / matrix.columns, 0.1f * (matrixSize.y - 2 * 10) / matrix.rows);
+        grid.padding = new RectOffset(10, 10, 10, 10);
+
+        GameObject tmpCell;
+        for (int i = 0; i < matrix.rows; i++)
+        {
+            for (int j = 0; j < matrix.columns; j++)
+            {
+                tmpCell = Instantiate(cellObjectPrefab);
+                tmpCell.transform.SetParent(forCells.transform);
+                tmpCell.name = "Cell (" + i + "," + j + ")";
+                tmpCell.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+                AssignToCell(i, j, tmpCell);
+            }
+        }
+
+        UpdateDisplay();
+    }
+
+    public void SetMatrixValue(MatrixCalc.Matrix _matrix)
+    {
+        if (matrix.rows != _matrix.rows || matrix.columns != _matrix.columns)
+            ResizeMatrix(_matrix.rows, _matrix.columns);
+
+        matrix = _matrix;
+        UpdateDisplay();
     }
 }
